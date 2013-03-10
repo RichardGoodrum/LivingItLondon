59fa5a4216423afd21f07c54bf25a5762040a10c
Richard Goodrum <RichardGoodrum1989@gmail.com>
2013-03-10 14:36:16 +0000

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Livingitlondon::Application.config.secret_token = '990feec5fd2c5eb11ad695d2dbd2fab648ab09bb94d9bfc62d734fb34c19deb7ebd21ac5343aec92397a0a48b006b9fec51e9708acf2164ac054c0e39076785b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Livingitlondon::Application.config.session_store :cookie_store, key: '_livingitlondon_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Livingitlondon::Application.config.session_store :active_record_store
